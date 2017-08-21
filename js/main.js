0f6c1e86e0929b39459406c6ce7097672c1f1258
dydh <deepthiyathiender@gmail.com>
2017-08-20 18:36:52 -0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -48,6 +48,61 @@ setTimeout(function () {
 }, 2500);
 
 
+$('input.check').on('change', function () {
+
+    $(".error").remove();
+
+    if (this.id == "attend4") {
+        $(".check").not(this).prop('checked', false);
+        $(".guests").hide();
+    }
+    else {
+        $(".guests").show();
+        if ($("#attend4").is(':checked')) {
+            $("#attend4").prop('checked', false);
+        }
+    }
+});
 $(".btn").click(function () {
     $(".both").toggleClass('flipped');
 });
+$("#cancel").click(function () {
+    $(".both").toggleClass('flipped');
+});
+
+function onLoad() {
+    $.getJSON('//freegeoip.net/json/?callback=?', function (data) {
+        $("#ip").val(data.ip);
+        $("#city").val(data.city);
+    });
+}
+
+$("form").submit(function (event) {
+    var name = $("#name").val();
+    var attend1 = $("#attend1").is(':checked');
+    var attend2 = $("#attend2").is(':checked');
+    var attend3 = $("#attend3").is(':checked');
+    var attend4 = $("#attend4").is(':checked');
+
+    var attend = attend1 || attend2 || attend3;
+    var regret = attend4;
+
+    var count = $('#count option:selected').text();
+
+    if (name!=="" && attend && count !== "Choose" || name && regret) {
+        alert("Thank you for your response!");
+        $(".both").toggleClass('flipped');
+        return;
+    }
+    $("#rsvp").append("<span class='error'>Please fill out all the fields above. Thank you!</span>");
+
+    $("#name").on('change', function () {
+        $(".error").remove();
+    });
+
+    $("#count").on('change', function () {
+        $(".error").remove();
+    })
+
+});
+
